befdb3f5d0f1cebef331ea0f0d503ffc93c8ee10
Sin0001 <bboyliljie@gmail.com>
2021-09-12 00:13:34 +0800

    Add show.handlebars

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index', {restaurant: restaurantList.results})
+  res.render('index', { restaurant: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
 })
 
 app.listen(port, () => {
