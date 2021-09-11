ab51ed9d4c6c8d76d866f373e9c36fad5ab41811
Sin0001 <bboyliljie@gmail.com>
2021-09-12 01:00:43 +0800

    Add search route

--- app.js
+++ app.js
@@ -14,8 +14,17 @@ app.get('/', (req, res) => {
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant: restaurant })
+  res.render('show', { restaurant })
 })
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurant: restaurants, keyword })
+})
+
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
